ee74ebe838b08ecff71a712935c1b313b00f8f41
Andreas Stehl <andreas.stehl@ista.com>
2020-12-05 16:38:45 +0100

    feat: read books from backendservice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './services/BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
   }
 
   render() {
+    console.log(`Books: [${this.state.books.length}] \n${JSON.stringify(this.state.books)}`);
     return (
       <div className="app">
         <Route path="/search" render={() => (
